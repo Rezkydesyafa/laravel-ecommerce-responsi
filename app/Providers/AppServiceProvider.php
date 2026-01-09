1b16f606c7b7904a93846119efc32e1f9ff5d32f
Rezky Desyafa <rezkydesyafa@gmail.com>
2026-01-09 08:04:00 +0700

    https provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan kode ini:
+        if (app()->environment('production') || app()->environment('local')) {
+            URL::forceScheme('https');
+        }
     }
 }
